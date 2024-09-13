75757160a92fb39b8555f9c448588b568750d4fe
R DOONDI GANGADHAR <160998038+doondigangadhar30@users.noreply.github.com>
2024-09-14 00:37:31 +0530

    commit done
    This Python program encrypts input text by shifting each character by a user-specified number of positions in the alphabet (shift value). This ensures the original message is scrambled for secure transmission.

---
+++ Day8/text encryption.py
@@ -0,0 +1,13 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text, shift):
+    original_text = ""
+    for letter in text:
+        shifted_position = alphabet.index(letter)+shift
+        cipher_text = alphabet[shifted_position]
+        original_text+= cipher_text
+    print(f"Encrypted text for {text} is: {original_text}")
+encrypt(text,shift)
